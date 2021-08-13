2b665521608872d0e25433f82c56980b76cfb102
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-13 21:33:43 +0530

    Adding: The invice generater should now take in multiple ride and calculate fair

--- main/java/com/bridgelabz/InvoiceGenerator.java
+++ main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,11 +1,34 @@
 package com.bridgelabz;
 
+/* @Description- calculate fare given distance and time invoice generator should return the
+ *   total fare of journey. */
 public class InvoiceGenerator {
+    private static final double NORMAL_MINIMUN_COST_PER_KILOMETER =10;
+    private static final int NORMAL_COST_PER_TIME = 1;
+    private static final double NORMAL_MINIMUN_FARE = 5;
+    private static final double PREMIUM_MINIMUN_COST_PER_KILOMETER =15;
+    private static final int PREMIUM_COST_PER_TIME = 2;
+    private static final double PREMIUM_MINIMUN_FARE = 20;
 
-    private final double COST_PER_KILOMETER = 10;
-    private final int COST_PER_MINUTE = 1;
     public double calculateFare(double distance, int time) {
-        return (distance * COST_PER_KILOMETER + time * COST_PER_MINUTE);
+        double NormaltotalFare =  distance * NORMAL_MINIMUN_COST_PER_KILOMETER  + time * NORMAL_COST_PER_TIME ;
+        if(NormaltotalFare < NORMAL_MINIMUN_FARE ) {
+            return  NORMAL_MINIMUN_FARE;
+        }
+        return NormaltotalFare;
     }
+    /* @Description -the invoice generator should now take in multiple rides  and calculate total fare */
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride:rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome cab invoice generator");
+    }
+}
 
--- test/java/com/bridgelabz/InvoiceServiceTest.java
+++ test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -3,14 +3,35 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InvoiceServiceTest {
     @Test
-    public void givenDistanceAndTime_ShouldReturnTrue(){
-        InvoiceGenerator invoiceGenator =new InvoiceGenerator();
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
-        int time =5;
-        double fare = invoiceGenator.calculateFare(distance, time);
-        Assertions.assertEquals(25,fare,0.0);
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.2;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare, 0.0);
     }
 }
-
