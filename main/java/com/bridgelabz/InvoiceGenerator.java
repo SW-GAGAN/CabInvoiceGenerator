af12abeca7dd1ff6acfea5bca321bab77ea7c3b4
SW-GAGAN <srgaganreddy0@gmail.com>
2021-08-13 10:11:44 +0530

    Adding: welcome Message

---
+++ main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+
+    private final double COST_PER_KILOMETER = 10;
+    private final int COST_PER_MINUTE = 1;
+    public double calculateFare(double distance, int time) {
+        return (distance * COST_PER_KILOMETER + time * COST_PER_MINUTE);
+    }
+    }
+
---
+++ test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTrue(){
+        InvoiceGenerator invoiceGenator =new InvoiceGenerator();
+        double distance = 2.0;
+        int time =5;
+        double fare = invoiceGenator.calculateFare(distance, time);
+        Assertions.assertEquals(25,fare,0.0);
+    }
+}
